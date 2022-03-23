5b860df1e0fc2f0196021e03657618c20b9250d8
StevanFreeborn <sfreeborn7@gmail.com>
2022-03-23 16:14:25 -0500

    Sharing links functional;

--- index.js
+++ index.js
@@ -19,21 +19,43 @@ function getQuotes(){
     });
 }
 
-// Select a quote randomly.
-function getQuote(){
-    let randomValue = Math.floor(Math.random() * data.quotes.length);
-    return data.quotes[randomValue];
-}
-
+// build image path.
 function getImagePath(authorName){
     let name = authorName.toLowerCase().replace(/\s/g, "-");
     return `images/authors/${name}.jpg`;
 }
 
+// Build image tumblr sharing link.
+function getTumblrLink(quoteText, authorName){
+    const baseUrl = "https://www.tumblr.com/widgets/share/tool?posttype=quote&canonicalUrl=https%3A%2F%2Fstevanfreeborn.github.io%2FfreeCodeCamp-Random-Quote-Machine&content=";
+    let textToShare = encodeURIComponent(quoteText) + "&title=" + encodeURIComponent(authorName);
+    let link = baseUrl + textToShare;
+    $("#tumblr-quote").attr("href", `${link}`);
+}
+
+// Build twitter sharing link.
+function getTwitterLink(quoteText, authorName) {
+    const baseUrl = "https://twitter.com/intent/tweet?text=";
+    let textToShare = encodeURIComponent('"' + quoteText + '"' + " - " + authorName);
+    let link = baseUrl + textToShare;
+    $("#tweet-quote").attr("href", `${link}`);
+}
+
+let imagePath = null;
+let quote = null;
+
+// Select a quote randomly.
+function getQuote(){
+    let randomValue = Math.floor(Math.random() * data.quotes.length);
+    quote = data.quotes[randomValue]
+    imagePath = getImagePath(quote.author);
+    getTumblrLink(quote.quote, quote.author);
+    getTwitterLink(quote.quote, quote.author);
+}
+
 // Get quote and author text on initial load.
 function displayQuoteAndAuthorText() {
-    let quote = getQuote();
-    let imagePath = getImagePath(quote.author);
+    getQuote();
     $("#background").css("background-image", `url(${imagePath})`);
     $("#text").text(quote.quote);
     $("#author").text("- " + quote.author);
@@ -42,9 +64,7 @@ function displayQuoteAndAuthorText() {
 
 // Get new quote and author text when new quote button is clicked.
 function getNewQuoteAndAuthorText() {
-    let quote = getQuote();
-    let imagePath = getImagePath(quote.author);
-    console.log(imagePath);
+    getQuote();
     $("#background").fadeOut("slow", function() {
         $(this).css("background-image", `url(${imagePath})`);
         $(this).fadeIn("slow");
